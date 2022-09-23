37f6d4d9af905e92036ab70dfe0f752030c68e7e
ccvnguyen <ccv.nguyen@soprasteria.com>
2022-09-23 13:16:03 +0200

    Init

---
+++ FunktionaleProgrammierung/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main(string [] args)
+    {
+        var myArray= new []{1,2,3,4,5,6,7,8,9};
+
+        foreach (var i in Filter(myArray, i => i%2==0 ))
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    private static IEnumerable<T> Filter<T>(
+        IEnumerable<T> source, 
+        Func<T,bool> filterCondition
+        )
+    {
+        foreach (var i in source)
+        {
+            if (filterCondition(i))
+            {
+                yield return i;
+            }
+        }
+    }
+}
+
+
